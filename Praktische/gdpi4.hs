ae9670d0a1a34f1377ddfcf331bd73b915c7b754
Sodutrii <bischoflaurin@gmail.com>
2022-12-19 13:21:48 +0100

    I emeber 2
    He did emember

---
+++ Praktische/gdpi4.hs
@@ -0,0 +1,9 @@
+data Rank = VII | VIII | IX | X | Jack | Queen | King | Ace
+    deriving (Show, Eq, Ord)
+
+
+data Suit = Clubs | Diamonds | Hearts | Spades
+    deriving (Show, Eq)
+
+data Card = Of Rank Suit
+    deriving Show
